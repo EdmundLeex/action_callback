3e1a0db8fcf560311b25dfd464964a91fe7795c4
Edmund Li <edmund.xz.lee@gmail.com>
2017-01-06 09:04:14 -0800

    test report

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in callback_action.gemspec
 gemspec
+
+gem 'minitest-reporters', group: :test
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'callback_action'
 
 require 'minitest/autorun'
+require 'minitest/reporters'
+
+reporter_options = { color: true }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
