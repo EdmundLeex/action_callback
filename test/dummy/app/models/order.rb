e50579c42cba3cc19a067556e865defcccf3818e
Edmund Li <edmund.xz.lee@gmail.com>
2017-01-13 17:08:53 -0800

    more tests for validation

--- test/dummy/app/models/order.rb
+++ test/dummy/app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ActiveRecord::Base
   validate :order_paid, before: [:ship]
+  validates :number, presence: true
+  validate :order_status_is_created, on: :create
 
   def ship
     update_column(:status, 'shipped')
@@ -7,5 +9,11 @@ class Order < ActiveRecord::Base
 
   def order_paid
     errors.add(:base, 'not paid')
+  end
+
+  def order_status_is_created
+    if status != 'created'
+      errors.add(:status, 'status should be created')
+    end
   end
 end
--- test/dummy/test/models/order_test.rb
+++ test/dummy/test/models/order_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "it doesn't run if there is error" do
+    order = Order.new
+
+    order.ship
+    assert_not_equal(order.status, 'shipped')
+    assert order.errors.full_messages.include?('not paid')
+  end
+
+  test "it doesn't ruin the ActiveRecord validation chain" do
+    order = Order.new
+    order.status = 'created'
+
+    assert !order.save
+    assert order.errors.present?
+
+    order.number = 'O123456'
+
+    assert order.save
+    assert !order.errors.present?
+  end
+
+  test "it doesn't ruin the ActiveRecord custom validation" do
+    order = Order.new
+    order.number = 'O123456'
+
+    assert !order.save
+    assert order.errors.present?
+
+    order.status = 'created'
+    assert order.save
+    assert !order.errors.present?
+  end
 end
